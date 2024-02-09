6127a223833fa05708419fd0fb5bf85c42745fec
Pan <kpkong@hk01.com>
2024-02-10 00:47:59 +0800

    feat: get query and path params example

--- src/handlers/get-demo/index.ts
+++ src/handlers/get-demo/index.ts
@@ -2,9 +2,9 @@
 import { minus } from "/opt/nodejs/services/test";
 import { add } from "/opt/nodejs/utils/test";
 
-import { Handler } from "aws-lambda";
+import { APIGatewayEvent, Handler } from "aws-lambda";
 
-export const handler: Handler = async (event, context) => {
+export const handler: Handler = async (event: APIGatewayEvent, context) => {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   console.log(`Expect add result "${add(1, 2)}" to be 3`);
   console.log(`Expect minus result "${minus(1, 2)}" to be -1`);
@@ -13,6 +13,8 @@ export const handler: Handler = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({
       message: "Hello from get-demo!",
+      queryParameters: event.queryStringParameters || {},
+      pathParameters: event.pathParameters || {},
       minus: minus(1, 2),
       add: add(1, 2),
     }),
