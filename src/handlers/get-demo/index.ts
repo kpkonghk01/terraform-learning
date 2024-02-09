7c4a64f9de6b9806e770c86d5ca3f778e07beda7
Pan <kpkong@hk01.com>
2024-02-10 00:44:19 +0800

    feat: handler example

--- src/handlers/get-demo/index.ts
+++ src/handlers/get-demo/index.ts
@@ -1,5 +1,20 @@
+// import from utils example
+import { minus } from "/opt/nodejs/services/test";
 import { add } from "/opt/nodejs/utils/test";
 
-console.log("hello world");
+import { Handler } from "aws-lambda";
 
-console.log(add(1, 2));
+export const handler: Handler = async (event, context) => {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  console.log(`Expect add result "${add(1, 2)}" to be 3`);
+  console.log(`Expect minus result "${minus(1, 2)}" to be -1`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from get-demo!",
+      minus: minus(1, 2),
+      add: add(1, 2),
+    }),
+  };
+};
